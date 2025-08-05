9b21e390cc321280a9b5cd18dd9de42667fdf864
x4p <trilicle@gmail.com>
2025-08-05 21:28:09 +0800

    stupidity

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/test/',
+  base: '/frame-attacher/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
